0dd42bfa4a00a1ff9620d9ec5161acfd2754a0ff
brigitteorina <brigitteorina@gmail.com>
2023-05-01 22:48:31 +0300

    Completed assignment

---
+++ bot/src/components/YourBotArmy.js
@@ -0,0 +1,27 @@
+import React from "react";
+import BotCard from "./BotCard"
+
+function YourBotArmy({ bots, removeBot , deleteBot }) {
+  //your bot army code here...
+
+  const mapBots = bots.map(bot => 
+    <BotCard 
+    key={bot.id}
+    bot={bot}
+    clickEvent={removeBot}
+    deleteBot={deleteBot}
+    />)
+
+  return (
+    <div className="ui segment inverted olive bot-army">
+      <div className="ui five column grid">
+        <div className="row bot-army-row">
+          {mapBots}
+          Your Bot Army
+</div>
+      </div>
+    </div>
+  );
+}
+
+export default YourBotArmy;
